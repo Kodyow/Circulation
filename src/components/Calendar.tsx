e189917b6afedf304342ebf006b9572686ffe2a8
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2021-12-07 21:57:24 -0800

    Update to calendar. image for home page.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,26 +3,9 @@ import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
 import './Calendar.css';
 // import dateFns from "date-fns";
 
-class Calendar extends React.Component {
-    // state = {
-    //     currentMonth: new Date(),
-    //     selectedDate: new Date()
-    //   };
-    
-    //   renderHeader() {}
-    
-    //   renderDays() {}
-    
-    //   renderCells() {}
-    
-    //   onDateClick = day => {};
-    
-    //   nextMonth = () => {};
-    
-    //   prevMonth = () => {};
-    
+class Calendar extends React.Component {   
       render() {
-        const datevalue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 2);
+        const datevalue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
         return (
           <div className="calendar">
             <CalendarComponent value={datevalue}></CalendarComponent>
