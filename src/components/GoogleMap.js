02a9b182e3aa28fdeb1a6c2255c5313bf7effc8f
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2021-12-10 16:09:11 -0800

    Partially intigrated database to Maps.

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,5 +1,6 @@
 import React, {useEffect,useState, Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import Geocode from 'react-geocode';
 import Axios from 'axios';
 
 const mapStyles = {
@@ -9,6 +10,7 @@ const mapStyles = {
 
 export class MapContainer extends Component {
   render() {
+      //<LocationData />
     return (
       <Map
         google={this.props.google}
@@ -16,8 +18,8 @@ export class MapContainer extends Component {
         style={mapStyles}
         initialCenter={
           {
-            lat: -1.2884,
-            lng: 36.8233
+            lat: LocationData.setLat,
+            lng: LocationData.setLong
           }
         }
       />
@@ -25,15 +27,27 @@ export class MapContainer extends Component {
   }
 }
 
-function Data() {
+function LocationData() {
     const [location, getLocation] = useState("");
-
-    useEffect(() => {
-        Axios.get("http://localhost:5000/events").then((response) => {
-            getLocation(response.data);
+    const [latitude, setLat] = useState();
+    const [longitude, setLong] = useState();
+    
+    /*useEffect(() => {
+        Axios.get("http://localhost:5000/calendar").then((response) => {
+            getEvent(response.data);
             console.log(response);
         });;
-    }, [location]);
+    }, [location]); */
+
+    useEffect(() => {
+        Geocode.fromAddress(location.data).then(
+          response => {
+            setLat(response.results[0].geometry.location.lat);
+            setLong(response.results[0].geometry.location.lng);
+            getLocation(response.data);
+          }
+        )}, [location]
+      )
 }
 
 export default GoogleApiWrapper({
